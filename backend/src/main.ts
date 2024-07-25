637f642fef9d767a1df08d3f807795bd36fe4b9f
jappoman <gabriele.tarenzi@gmail.com>
2024-07-25 11:42:01 +0200

    wip on frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Configurazione CORS
+  app.enableCors({
+    origin: 'http://localhost:8080', // Indirizzo del frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Metodi HTTP permessi
+    allowedHeaders: ['Content-Type', 'Authorization'], // Intestazioni permessi
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
